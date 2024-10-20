921a22bf3451acaa27004f0668851d89553c2fa0
jarvis <168496656+jarvis630@users.noreply.github.com>
2024-10-20 10:31:00 +0530

    Update passoneprogram.c

--- passone/passoneprogram.c
+++ passone/passoneprogram.c
@@ -2,70 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main(){
-    char label[10],opcode[10],operand[10];
-    char code[10],mnemonic[10];
-    int start,locctr;
-    FILE *input,*output,*optab,*symtab;
-    
-    input = fopen("input.txt","r");
-    optab = fopen("optab.txt","r");
-    output = fopen("output.txt","w");
-    symtab = fopen("symtab.txt","w");
-    
-    fscanf(input,"%s\t%s\t%s",label,opcode,operand);
-    if(strcmp(opcode,"START") == 0){
-        start = atoi(operand);
-        locctr = start;
-        fprintf(output,"\t%s\t%s\t%s\n",label,opcode,operand);
-        fscanf(input,"%s\t%s\t%s",label,opcode,operand);
+void main() {
+    char label[10], operand[10], opcode[10];
+    char code[10], mnemonic[10];
+    int start, locctr;
+    FILE *input, *optab, *output, *symtab;
+
+    input = fopen ("input.txt", "r");
+    optab = fopen ("optab.txt", "r");
+    output = fopen ("output.txt", "w");
+    symtab = fopen ("symtab.txt", "w");
+
+    fscanf (input, "%s\t%s\t%s", label, opcode, operand);
+    if (strcmp (opcode , "START") == 0) {
+        locctr = atoi (operand);
+        start = locctr;
+        fprintf (output, "\t%s\t%s\t%s\n",label, opcode, operand);
+        fscanf (input, "%s\t%s\t%s",label, opcode, operand);
+    } else {
+        locctr = 0;
+        start = 0;
     }
-    else{
-        start = locctr = 0;
-    }
-    while(strcmp (opcode,"END") != 0){
-        fprintf(output,"%d\t",locctr);
-        
-        if(strcmp(label,"") !=0 && strcmp(label,"**") != 0){
-            fprintf(symtab,"%s\t%d\n",label,locctr);
+
+    while (strcmp (opcode, "END") != 0) {
+        fprintf (output, "%d\t",locctr);
+
+        if (strcmp (label, "") != 0 && strcmp (label, "**") != 0) {
+            fprintf (symtab, "%s\t%d\n",label, locctr);
         }
-        
-        fscanf(optab,"%s\t%s",code,mnemonic);
-        while(strcmp(code,"END") != 0){
-            if(strcmp(opcode,code) == 0){
+
+        fscanf (optab, "%s\t%s",code, mnemonic);
+        while (strcmp (code, "END") != 0) {
+            if (strcmp (code, opcode) == 0) {
                 locctr += 3;
+                break;
             }
-            fscanf(optab,"%s\t%s",code,mnemonic);
+            fscanf (optab, "%s\t%s",code, mnemonic);
         }
-        
-        if(strcmp(opcode,"WORD") == 0){
+
+        if (strcmp (opcode, "WORD") == 0)
             locctr += 3;
-        }
-        else if(strcmp(opcode,"RESW") == 0){
-            locctr += 3*atoi(operand);
-        }
-        else if(strcmp(opcode,"RESB") == 0){
-            locctr += atoi(operand);
-        }
-        else if(strcmp(opcode,"BYTE") == 0){
-            ++locctr;
-        }
-        
-        fprintf(output,"\t%s\t%s\t%s\n",label,opcode,operand);
-        fscanf(input,"%s\t%s\t%s",label,opcode,operand);
+        else if (strcmp (opcode, "RESW") == 0)
+            locctr += (3 * atoi (operand));
+        else if (strcmp (opcode, "RESB") == 0)
+            locctr += atoi (operand);
+        else if (strcmp (opcode, "BYTE") == 0)
+              ++locctr;
+
+        fprintf (output, "\t%s\t%s\t%s\n",label, opcode, operand);
+        fscanf (input, "%s\t%s\t%s",label, opcode, operand);
     }
-    fprintf(output,"\t%s\t%s\t%s",label,opcode,operand);
-    printf("\nProgram size is %d",locctr - start);
-    
+
+    fprintf (output, "%d\t%s\t%s\t%s\n",locctr,label, opcode, operand);
+    printf("\nProgram Size : %d\n", locctr - start);
+
     fclose (input);
     fclose (optab);
     fclose (output);
     fclose (symtab);
-
 }
-
-
-
-
-
-
