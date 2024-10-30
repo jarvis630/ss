28752773b9c81a714fe0265f0c0aae3d109cc44a
jarvis <168496656+jarvis630@users.noreply.github.com>
2024-10-30 11:34:18 +0530

    Update relocationloader.c

--- relocationaromal/relocationloader.c
+++ relocationaromal/relocationloader.c
@@ -1,4 +1,4 @@
-#include <stdio.h>                                                                                                                                                                                 relocation_loader.c                                                                                                                                                                                                          #include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -64,4 +64,32 @@ int main() {
 
     return EXIT_SUCCESS;
 }
+
+void convert(const char *hex, char *binary) {
+    binary[0] = '\0'; // Clear binary output
+    for (int i = 0; hex[i] != '\0'; i++) {
+        switch (hex[i]) {
+            case '0': strcat(binary, "0000"); break;
+            case '1': strcat(binary, "0001"); break;
+            case '2': strcat(binary, "0010"); break;
+            case '3': strcat(binary, "0011"); break;
+            case '4': strcat(binary, "0100"); break;
+            case '5': strcat(binary, "0101"); break;
+            case '6': strcat(binary, "0110"); break;
+            case '7': strcat(binary, "0111"); break;
+            case '8': strcat(binary, "1000"); break;
+            case '9': strcat(binary, "1001"); break;
+            case 'A': strcat(binary, "1010"); break;
+            case 'B': strcat(binary, "1011"); break;
+            case 'C': strcat(binary, "1100"); break;
+            case 'D': strcat(binary, "1101"); break;
+            case 'E': strcat(binary, "1110"); break;
+            case 'F': strcat(binary, "1111"); break;
+            default:
+                printf("Invalid hex character: %c\n", hex[i]);
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
 
